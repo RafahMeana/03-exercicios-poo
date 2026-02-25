f680bf27068fd96eb781f3d975c609e6cfff5fcb
RAFINHAMOREIRA08 <rafah.bacha@gmail.com>
2026-02-25 11:37:37 -0300

    Exercicio 1 finalizado

---
+++ src/exercicio01/Conta.java
@@ -0,0 +1,15 @@
+package exercicio01;
+
+public class Conta {
+    String correntista;
+    int numero;
+    double saldo;
+
+    public void depositar(double valor) {
+        saldo = saldo + valor;
+    }
+
+    public void sacar(double valor) {
+        saldo = saldo - valor;
+    }
+}
--- src/exercicio01/Main.java
+++ src/exercicio01/Main.java
@@ -1,4 +1,30 @@
 package exercicio01;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Conta conta = new Conta();
+        Random rd = new Random();
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        double valor;
+
+        System.out.println("Correntista -->");
+        conta.correntista = sc.next();
+        conta.numero = rd.nextInt(1000, 9999);
+
+        System.out.println("Saldo inicial R$ " + df.format(conta.saldo));
+        System.out.println("valor para depósito R$ " );
+        valor = sc.nextDouble();
+        conta.depositar(valor);
+        System.out.println("Saldo atual R$ " + df.format(conta.saldo));
+        System.out.println("valor para saque R$ ");
+        valor = sc.nextDouble();
+        conta.sacar(valor);
+        System.out.println("Saldo atual R$ " + df.format(conta.saldo));
+    }
 }
